0313e12cdcfb433bf2fbd8d0ebf7b71795372f1a
Darly Julieth <darlyhoyos65@gmail.com>
2026-04-05 21:03:38 -0500

    THE END PROJECT

--- ExpressionEvaluator.UI.Console/Program.cs
+++ ExpressionEvaluator.UI.Console/Program.cs
@@ -6,6 +6,7 @@ try
     var result2 = Evaluator.Evaluate("2*7/4-(8-9^(1/2))+6");
     var result3 = Evaluator.Evaluate("4*(5+6-(8/2^3)-7)-1");
 
+
     Console.WriteLine(result1);
     Console.WriteLine(result2);
     Console.WriteLine(result3);
